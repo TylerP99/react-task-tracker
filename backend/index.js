128e4616e752ad86993a2a8ee56455371c9293de
Tyler Perata <tylerperata@gmail.com>
2022-11-18 15:57:41 -0800

    Add middleware

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,22 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 require("./config/connectMongo")();
 
 const passport = require("passport");
 require("./config/passportConfig")(passport);
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.MONGO_URI}),
+}));
 
 app.use("/", require("./routes/index"));
 
